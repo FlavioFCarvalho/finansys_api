e2a1c266d539f5cb32170403db692b3eb17c603e
Flávio Carvalho <flaviocfr@gmail.com>
2019-08-16 10:20:34 -0300

    Criando a operação de instaciação de Categoria

--- finansys/src/main/java/com/finansys/FinansysApplication.java
+++ finansys/src/main/java/com/finansys/FinansysApplication.java
@@ -1,13 +1,32 @@
 package com.finansys;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.finansys.domain.Categoria;
+import com.finansys.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class FinansysApplication {
+public class FinansysApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository; 
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinansysApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null,"Informática",null);
+		Categoria cat2 = new Categoria(null,"Escritório",null);
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
