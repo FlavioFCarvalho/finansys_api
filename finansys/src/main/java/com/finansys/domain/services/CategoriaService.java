79615877e89eb290f7982babe2900ff3d6ae35b6
Flávio Carvalho <flaviocfr@gmail.com>
2019-09-03 15:04:12 -0300

    Validação sintática com Bean Validation

--- finansys/src/main/java/com/finansys/domain/services/CategoriaService.java
+++ finansys/src/main/java/com/finansys/domain/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.finansys.domain.Categoria;
+import com.finansys.domain.dto.CategoriaDTO;
 import com.finansys.domain.services.exceptions.DataIntegrytiException;
 import com.finansys.domain.services.exceptions.ObjectNotFountException;
 import com.finansys.repositories.CategoriaRepository;
@@ -48,6 +49,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(),objDTO.getName(), null);
 	}
 
 }
