e2749065aa79fe45492e290a76a043a45e61c8fa
Flávio Carvalho <flaviocfr@gmail.com>
2019-09-04 16:52:18 -0300

    PUT, DELETE e GET para Cliente

--- finansys/src/main/java/com/finansys/domain/services/ClienteService.java
+++ finansys/src/main/java/com/finansys/domain/services/ClienteService.java
@@ -1,11 +1,19 @@
 package com.finansys.domain.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.finansys.domain.Cliente;
+import com.finansys.domain.Cliente;
+import com.finansys.domain.dto.ClienteDTO;
+import com.finansys.domain.services.exceptions.DataIntegrytiException;
 import com.finansys.domain.services.exceptions.ObjectNotFountException;
 import com.finansys.repositories.ClienteRepository;
 
@@ -19,6 +27,39 @@ public class ClienteService {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFountException(
 		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);	
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrytiException("Não é possivél excluir porque há entidades relacionadas");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDTO) {
+		return new Cliente(objDTO.getId(),objDTO.getNome(),objDTO.getEmail(),null,null );
+	}
+
+	public Cliente update(Cliente obj) {
+		Cliente objNew = find(obj.getId());
+		updateData(objNew, obj);
+		return repo.save(objNew);
+	}
+
+	private void updateData(Cliente objNew, Cliente obj) {
+		objNew.setNome(obj.getNome());
+		objNew.setEmail(obj.getEmail());
 	}
 
 }
