70d2e841965fcff0dcffe0118c305e06494afbd5
Flávio Carvalho <flaviocfr@gmail.com>
2019-10-11 16:20:00 -0300

    Busca de pedidos por nome e categorias

--- finansys/src/main/java/com/finansys/repositories/ProdutoRepository.java
+++ finansys/src/main/java/com/finansys/repositories/ProdutoRepository.java
@@ -1,11 +1,21 @@
 package com.finansys.repositories;
 
+import java.util.List;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.finansys.domain.Categoria;
 import com.finansys.domain.Produto;
 
 @Repository
 public interface ProdutoRepository  extends JpaRepository<Produto, Integer>{
 
+	
+	@Transactional(readOnly = true)
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
 }
